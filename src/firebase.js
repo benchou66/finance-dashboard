fb1eae1cb00165423e16b8642dfea47ab2b287ed
benchou66 <129167527+benchou66@users.noreply.github.com>
2026-03-11 11:06:06 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:            "AIzaSyCVXWZc3mT8EtPHa27adswSJ6h58fDlW_4",
+  authDomain:        "firestore-database-e7511.firebaseapp.com",
+  projectId:         "firestore-database-e7511",
+  storageBucket:     "firestore-database-e7511.firebasestorage.app",
+  messagingSenderId: "1038831129421",
+  appId:             "1:1038831129421:web:05e81b2942a089793b9d04",
 };
 
 const app = initializeApp(firebaseConfig);
